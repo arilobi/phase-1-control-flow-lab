b962de65d7c5e9d6bcda0808212b298c3796918b
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-02 15:11:26 +0300

    add control flow

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (distance<400) {
+    return 'This one is on me!'
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  else if(distance>400 && distance<=2000) {
+    return 'That will be twenty bucks.'
+  }
+
+  else if(distance>2000 && distance<=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else{
+    return 'No can do.'
+  }
 }
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
+
+function ternaryCheckCity(city){
+  return city==="NYC" ? 'Ok, sounds good.' : 'No go.'
+}
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Pittsburgh'));
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+
+    case 'not as generous':
+      return 'Thank you.';
+
+    default:
+      return 'Bye.';
+  }
+}
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thanks for everything'));
